f6aa178bde3e2b557185c450c52919d7c40d20d2
Diego Neves <pdcassiano@gmail.com>
2022-10-10 16:59:32 -0300

    ajustes no layout

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const name = document.getElementById('name')
 const email = document.getElementById('email')
 const button = document.getElementById('button')
 const form = document.getElementById('form')
+const ops = document.getElementById('ops')
 
 
 function check() {
     if(name.value != '' && email.value != '') {
         button.classList.remove('invalid')
+        ops.classList.remove('invalid')
     } else {
-        button.classList.add('invalid')
         mudarDeLugar()
+        button.classList.add('invalid')
+        ops.classList.add('invalid')
     }
 }
 
@@ -17,6 +20,10 @@ function mudarDeLugar() {
 let numeroAleatorio = Math.floor(Math.random() * form.clientWidth) - button.clientWidth
     if (numeroAleatorio > 0) {
         button.style.marginLeft = numeroAleatorio + 'px'
+        ops.style.marginLeft = numeroAleatorio + 'px'
+        setTimeout(() => {
+            ops.classList.remove('invalid')
+        }, 500)
     } else {
         button.style.marginLeft = 0 + 'px'
     }
